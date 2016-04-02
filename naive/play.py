b8fda73d59f0cf916517d89605c6d02815c681c1
Eero af Heurlin <eero.afheurlin@iki.fi>
2016-04-02 13:08:37 +0300

    keep track of the zmq state

--- naive/play.py
+++ naive/play.py
@@ -8,7 +8,7 @@ import sys,os,time
 from chasegenerator import Chasepattern
 from apa102 import colors2frame
 
-DEBUG=True
+DEBUG=False
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -16,6 +16,7 @@ if __name__ == '__main__':
         sys.exit(1)
     
     pattern = Chasepattern(int(sys.argv[2]))
+    pattern.forever = True
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect(sys.argv[1])
@@ -26,34 +27,38 @@ if __name__ == '__main__':
     if len(sys.argv) >= 5:
         pattern.chasecolor = chroma.Color(sys.argv[4])
     
-    delay = 0.025
+    delay = 0.005
     if len(sys.argv) >= 6:
         delay = float(sys.argv[5])
 
     try:
-        try:
-            sent = False
-            while True:
-                tstart = time.time()
-                if sent:
-                    # We must read the reply even though it's empty
-                    socket.recv()
+        doread = False
+        while True:
+            tstart = time.time()
+            if doread:
+                # We must read the reply even though it's empty
+                socket.recv()
+                doread = False
+            try:
                 frame = colors2frame(pattern.__next__())
-                adelay = (tstart+delay) - time.time()
-                if adelay > 0:
-                    time.sleep(adelay)
-                else:
-                    if DEBUG:
-                        print("%f: Missed deadline!" % time.time())
+            except StopIteration:
+                break
+            adelay = (tstart+delay) - time.time()
+            if adelay > 0:
+                time.sleep(adelay)
+            else:
                 if DEBUG:
-                    print("%f: sending: %s" % (tstart, repr(frame)))
-                socket.send(frame)
-                sent = True
-        except StopIteration:
-            pass
+                    print("%f: Missed deadline!" % time.time())
+            if DEBUG:
+                print("%f: sending: %s" % (tstart, repr(frame)))
+            socket.send(frame)
+            doread = True
     except KeyboardInterrupt:
         pass
 
+    if doread:
+        socket.recv()
+        doread = False
     socket.send(colors2frame([chroma.Color("#000000")]*pattern.numleds))
     # We must read the reply even though it's empty
     socket.recv()
